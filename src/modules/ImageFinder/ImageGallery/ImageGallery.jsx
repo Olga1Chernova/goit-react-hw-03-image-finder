a08d6264c9f2dd70f353c485af1eef222c063c85
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-02-13 10:09:43 +0200

    Update ImageGallery.jsx

--- src/modules/ImageFinder/ImageGallery/ImageGallery.jsx
+++ src/modules/ImageFinder/ImageGallery/ImageGallery.jsx
@@ -1,24 +1,30 @@
-import css from "./image-gallery.module.scss";
-import ImageGalleryItem from "../ImageGalleryItem/ImageGalleryItem";
+import PropTypes from 'prop-types';
 
-const ImageGallery = ({items, showImg}) => {
-    return (
-      <ul className={css.ImageGallery}>
-        {items.map(({ id, webformatURL, largeImageURL, tags }) => (
-          <ImageGalleryItem
-            showImg={showImg}
-            key={id}
-            webformatURL={webformatURL}
-            largeImageURL={largeImageURL}
-            tags={tags}
-          />
-        ))}
-      </ul>
-    );
-}
+import css from './image-gallery.module.scss';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+
+const ImageGallery = ({ items, showImg }) => {
+  return (
+    <ul className={css.ImageGallery}>
+      {items.map(({ id, webformatURL, largeImageURL, tags }) => (
+        <ImageGalleryItem
+          showImg={showImg}
+          key={id}
+          webformatURL={webformatURL}
+          largeImageURL={largeImageURL}
+          tags={tags}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default ImageGallery;
 
 ImageGallery.defaultProps = {
   items: [],
+};
+
+ImageGallery.propTypes = {
+  showImg: PropTypes.func.isRequired,
 }
